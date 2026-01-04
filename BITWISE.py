013ec8bc95490cbeccf9b726ebcb82868d2419e6
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-04 23:49:07 +0530

    BITWISE

---
+++ BITWISE.py
@@ -0,0 +1,12 @@
+#........BITWISE.........#
+#&,|,^,~,<<,>>
+#Python Bitwise operators act on bits and perform bit-by-bit operations. These are used to operate on binary numbers.
+#AND 
+print(7 & 7)
+print(4 & 5)
+print(0 & 1)
+print(1 & 0)
+a=11
+b=6
+print(a & b)
+#or
