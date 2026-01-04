510bfaaa7fb473bbcd74974858d0d0ba46e1afab
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-04 23:12:14 +0530

    LOGICAL

---
+++ LOGICAL.py
@@ -0,0 +1,31 @@
+#...........LOGICAL..............#
+#Logical operators are used to combine or modify conditions and they always return a Boolean value (True or False).
+#AND 
+a=10
+b=15
+print(a>=b and a<=b)
+print()
+#OR
+print(a!=b or a<=b)
+#NOT
+print(not a)
+#another way x and y >y is the output if x is not equal to zero
+print(10 and 20) 
+print(0 and 20)
+print(90 and 0)
+print(False and False) 
+##T=1 F=0
+print(True or False)
+print(False or True)
+print(True or True)
+print(False or False)
+#if first number is non zero then it give it first either it is 4,6,-1
+print(4 or 6)
+print(-1 or -2)
+print(10 or 20)
+print(100 or 200)
+print(bool(40))
+print(not 8)
+print(not -1)
+
+
--- RELATIONAL.py
+++ RELATIONAL.py
@@ -8,3 +8,5 @@ print("a==b:" ,a==b)
 print("a!=b:", a!=b)
 print("a>=b:" ,a>=b)
 print("a<=b:", a<=b)
+#Uppercase letters have lower Unicode values than lowercase letters.
+#Python compares strings lexicographically, that is character by character from left to right using Unicode (ASCII) values.
