ce0bbe08220528bb59029595172cf772bcda0a45
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-04 22:18:49 +0530

    RELATIONAL

---
+++ RELATIONAL.py
@@ -0,0 +1,10 @@
+#.............RELATIONAL.......#
+#comparison or relational operators compare values .it may true or false
+a="vaishu"
+b="reddy"
+print("a>b:", a>b)
+print("a<b" ,a<b)
+print("a==b:" ,a==b)
+print("a!=b:", a!=b)
+print("a>=b:" ,a>=b)
+print("a<=b:", a<=b)
--- operators.py
+++ operators.py
@@ -17,7 +17,7 @@ print("DIVISION:" ,c)
 #MODULUS
 c=a%b
 print("MODULUS:" ,c)
-#EXPONENTATION
+#EXPONENTATION//Python 3.x the result of division is a floating-point while in Python 2.x division of 2 integers was an integer. To obtain an integer result in Python 3.x floored (// integer) is used.
 c=a**b
 print("EXPONENTATION:" ,c)
 #FLOOR DIVISION
