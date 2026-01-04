4088c0566ebab4fe0f2b60f2cb660cf8c32c29b4
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-04 16:46:35 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,35 @@
+#.............OPERATORS..............#
+       #......ARITHMETIC......#
+#ADDITION       
+a=15
+b =4 
+c=a+b  
+print("ADDITION:" ,c)   
+#SUBTRACTION
+c=a-b
+print("SUBTRACTION:" ,c)
+#MULTIPLICATION
+c=a*b
+print("MULTIPLICATION:" ,c)
+#DIVISION
+c=a/b
+print("DIVISION:" ,c)
+#MODULUS
+c=a%b
+print("MODULUS:" ,c)
+#EXPONENTATION
+c=a**b
+print("EXPONENTATION:" ,c)
+#FLOOR DIVISION
+c=a//b
+print("FLOOR DIVISION:" ,c)
+
+
+
+
+
+
+
+
+ 
+       
