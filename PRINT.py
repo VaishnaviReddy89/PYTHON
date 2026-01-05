42ca635122613c384f4186c1f27bb56d7dca8a3e
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-05 22:27:49 +0530

    PRINT

--- PRINT.py
+++ PRINT.py
@@ -1,2 +1,31 @@
+print("***2 PRINT IN ONE LINE*****")
 print("hello world" ,end=" ")
 print("i will print on the same line.")
+
+#if you want to specify the datatype of a variable.
+x=str(3)
+y=int(3)
+z=int(3)
+
+print(x)
+print(y)
+print(z)
+
+print("****if you want to get the type********")
+x=5
+y="VAISHU"
+print(type(x))
+print(type(y))
+
+print("*****Many values to multiple Variable*******")
+x,y,z="organe","Banana","Cheery"
+print(x)
+print(y)
+print(z)
+#Python allows you to assign values to multiple variables in one line:
+
+print("******One Value to Multiple Variables******")
+x=y=z
+print(x)
+print(y)
+print(z)
