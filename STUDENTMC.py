a02c12090d2c38b1acc536af5109e60adda0e283
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-06 22:47:23 +0530

    STUDENTMC

---
+++ STUDENTMC.py
@@ -0,0 +1,10 @@
+total=0
+
+def calculate(DM,cosm,m1):
+      global total
+      total =DM+cosm+m1
+      average=total/3
+      print("total marks" ,total)
+      print("Äverage marks" ,average)
+
+calculate(35,55,77)
