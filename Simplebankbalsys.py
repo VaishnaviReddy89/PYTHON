586194cb0b53c11f1e48b7c2ba698b34ee98f01f
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-06 23:06:33 +0530

    Simplebankbalsys

---
+++ Simplebankbalsys.py
@@ -0,0 +1,14 @@
+balance = 1000   # global variable
+
+def deposit(amount):
+    global balance
+    balance += amount
+    print("Balance after deposit:", balance)
+
+def withdraw(amount):
+    global balance
+    balance -= amount
+    print("Balance after withdrawal:", balance)
+
+deposit(500)
+withdraw(300)
