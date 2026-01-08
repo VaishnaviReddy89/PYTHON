27125b737862a84c3022767ebef9591a68496f6d
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-08 22:58:46 +0530

    SETtypes

--- SETtypes.py
+++ SETtypes.py
@@ -3,3 +3,15 @@ x = {"apple", "banana", "cherry"}
 print(x)
 #display the data type of x:
 print(type(x)) 
+
+x = frozenset({"apple", "banana", "cherry"})
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+x = True
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
