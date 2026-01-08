3b2abd77473fa1da4b1da09599dbacaac948a567
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-08 22:14:52 +0530

    SEQUENCEdt

---
+++ SEQUENCEdt.py
@@ -0,0 +1,14 @@
+print("********SEQUENCETYPE*******")
+
+print("******LIST******")
+x=["apple","banana","cherry"]
+print(x)
+print(type(x))
+
+print("********TUPLE******")
+y=("apple","banana","cherry")
+print(type(y))
+
+print("****range********")
+z=range(9)
+print(list(z))
