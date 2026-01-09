e1a7a4fa33e47beb631924bf7f3d8673bfdec045
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-09 22:31:50 +0530

    bytearray

--- MAPPING.py
+++ MAPPING.py
@@ -1,6 +1,5 @@
 print("*******MAPPING******")
 #A mapping data type stores data in key–value pairs, where each key maps to a value.
 x = {"name" : "vaishu", "age" : 36}
-x={"name" :"vaishu","age" : 20}
 print(x)
 print(type(x))
---
+++ bytearray.py
@@ -0,0 +1,11 @@
+print("*********BYTEARRAY**********")
+#you can change its value after creation(MUTABLE)
+#used to store binary data
+#each value is an integer 0 to 225
+
+#with size(initializes with 0)
+x = bytearray(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
