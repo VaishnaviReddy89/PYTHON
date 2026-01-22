88232c1b6c89c32e3df8d9bd4b48b13172311349
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-22 23:07:20 +0530

    conversiondatatype

---
+++ conversiondatatype.py
@@ -0,0 +1,11 @@
+print("conversion of one datatype to another datatype")
+print("int to float")
+A=2
+print(float(A))
+print("$$$$$$$$$$$$")
+print("float to int")
+b=5.5
+print(int(b))
+
+#data loss ->explicit type conversion
+#no data loss->implict type conversion
