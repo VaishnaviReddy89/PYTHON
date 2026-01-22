ef7743b5902bc1681dc7f4a93ffa8a731c41098d
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-22 23:12:42 +0530

    conversiondatatype

--- conversiondatatype.py
+++ conversiondatatype.py
@@ -6,6 +6,12 @@ print("$$$$$$$$$$$$")
 print("float to int")
 b=5.5
 print(int(b))
-
+print("$$$$$$$$$$$$")
+print("int to complex")
+c=7
+print(complex(c))
 #data loss ->explicit type conversion
 #no data loss->implict type conversion
+#✔ int() removes decimal part
+#✔ float() adds .0 if needed
+#✔ complex() adds imaginary part
