848c91494cec8699c67c7c4418f3e1236ca39695
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-22 22:35:57 +0530

    unpackcollection

---
+++ unpackcollection.py
@@ -0,0 +1,13 @@
+print("******UNPACK COLLECTION********")
+
+print("LIST")
+marks=[10,20,30]
+vaishu,pandu,chinnu=marks
+print(vaishu)
+print(pandu)
+print(chinnu)
+print("/////////")
+print("TUPLE")
+fruits=("apple","orange")
+x,y=fruits
+print(x,y)
