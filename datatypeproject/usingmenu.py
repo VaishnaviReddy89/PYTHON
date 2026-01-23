f8ed57fe0e682d1f913fe047a17ecd2886278dcc
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-23 23:00:00 +0530

    usingmenu

--- datatypeproject/usingmenu.py
+++ datatypeproject/usingmenu.py
@@ -2,13 +2,24 @@ print("+-------STUDENT MARKS PROCESSING-------+")
 MARKS=input("ENTER STUDENTS MARKS: ")
 
 print("ENTER CONVERSION TYPE")
-print("1.YOU TO CONVERT TO INTEGER THEN CHOOSE THESE OPTION")
-print("2.YOU TO CONVERT TO FLAOT THEN CHOOSE THESE OPTION")
+print("1.IF YOU WANT TO CONVERT TO INTEGER THEN CHOOSE THESE OPTION")
+print("2.IF YOU WANT TO CONVERT TO FLOAT THEN CHOOSE THESE OPTION")
 
 choice=int(input("enter a choice: "))
 if choice==1:
     print("INTEGER VALUE: ", int(float(MARKS)))
 elif choice==2:
-    print("FLOAT MARKS: ",float(MARKS))
+    print("FLOAT VALUE: ",float(MARKS))
 else:
     print("YOU ARE CHOOSEN INVALID CHOICE SO PLEASE TRY AGAIN!!!!")
+#✔️
+
+#🧠 Rule to remember
+
+#Whenever input may contain decimals:
+
+#Always convert string → float → int
+
+#Never do:
+
+#string → int → float
