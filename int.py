3b46506c12eae671446e013ec581ed861c046713
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-01-25 23:40:11 +0530

    int

---
+++ int.py
@@ -0,0 +1,7 @@
+x = 1
+y = 35656222554887711
+z = -3255522
+#Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+print(type(x))
+print(type(y))
+print(type(z))
