379a91bb6d3091565a9b6381a22249b67c49c282
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-06 21:53:37 +0530

    rangeofindex

---
+++ Sequencetype.py/rangeofindex.py
@@ -0,0 +1,11 @@
+print("RANGE OF INDEX")
+tuple=("samsung","iphone","redmi","realme","google pixel")
+print(tuple[1:3])
+print("NEGATIVE INDEXING")
+#Negative indexing means start from the end. -1 refers to the last item, -2 refers to the second last item etc.
+tuple=("iphone","redmi","realme")
+print(tuple[-1])
+print("range of negative indexes")
+TUPLE=("APPLE","ORANGE","CHERRY","KIWI")
+print(TUPLE[-3:-1])
+
