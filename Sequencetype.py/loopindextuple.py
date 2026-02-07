d43eca4fcfdd8631d3d04dd80bb818ef4faf0e83
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-07 22:59:39 +0530

    check2

--- Sequencetype.py/loopindextuple.py
+++ Sequencetype.py/loopindextuple.py
@@ -1,4 +1,7 @@
 tuple=("apple","banana","cherry")
 for x in range(len(tuple)):
     print(tuple[x])
+    #ou can also loop through the tuple items by referring to their index number.
+
+#Use the range() and len() functions to create a suitable iterable.
     
