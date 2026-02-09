1958e6721b09890049ff9832efb0560687ca4a76
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-09 22:40:13 +0530

    list2

---
+++ Sequencetype.py/list2.py
@@ -0,0 +1,19 @@
+print("%%%LIST%%")
+V=[]
+print(type(V))
+numbers=[1,2,3,4]
+names=["vaish","reddy"]
+print("%%%%%%%%%%")
+print("positive index")
+mixed=[1,"python",4.5,True]
+print("%%%%%%%%%%")
+print("negative index")
+mixed1=[1,"python",3.5,True]
+print(numbers)
+print(names)
+print(mixed[-1])
+print(mixed1[1])
+print("$$$$$$$$$$$$$$")
+print("SLICING")
+mix=[1,"python",3.5,True,3,7]
+print(mix[0:4:1])
