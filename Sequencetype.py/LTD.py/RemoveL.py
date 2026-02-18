697fe70a5d4811a7f06cb693b80364f1759aa040
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-18 22:46:23 +0530

    RemoveL

---
+++ Sequencetype.py/LTD.py/RemoveL.py
@@ -0,0 +1,8 @@
+lst=[10,20,30,40,40]
+new_lst=[] #create empty list to store unique values
+for x in lst:
+    if x  not in new_lst:
+        new_lst.append(x) #We add the element only if it is not already there.
+        
+print(new_lst)
+        
