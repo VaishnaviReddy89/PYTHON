72124c31b9e2539d111eee57fc3f668df3658d88
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-18 22:32:13 +0530

    mergeD

--- Sequencetype.py/LTD.py/count.py
+++ Sequencetype.py/LTD.py/count.py
@@ -1,6 +1,6 @@
 data=[1,2,2,3,3,3,4]
 result={}
 for i in data:
-    result[i]=result.get(i,0)+1
-    print(result)
+    result[i]=result.get(i,0)+1 #get(i, 0) returns the current count of i if it exists, otherwise 0, then we add 1.
+print(result)
     
