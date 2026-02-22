751653882ec7cfca131ea80156da2380913a1f5d
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-22 15:22:40 +0530

    print

--- assignment/PYTHON/print.py
+++ assignment/PYTHON/print.py
@@ -2,3 +2,6 @@ print("$$$$$print without a new line$$$$")
 #two print in one line
 print("start learning" ,end=" ")
 print("python from today itself")
+
+print("MIX NUMBERS")
+print("IAM",20,"YEARS OLD")
