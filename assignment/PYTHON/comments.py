3d45d597798f4a88bd8ac77d506f85d4126a9ec9
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-22 15:29:37 +0530

    comments

---
+++ assignment/PYTHON/comments.py
@@ -0,0 +1,18 @@
+print("$$$$Python Comments$$$$")
+#Comments can be used to explain Python code.
+
+#Comments can be used to make the code more readable.
+
+#Comments can be used to prevent execution when testing code.
+
+print("Hello World")
+#the above print statement is comment
+
+
+#Since Python will ignore string literals that are not assigned to a variable, you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+"""
+this is a commeny
+written in
+more than just one line
+"""
+print("hello world")
