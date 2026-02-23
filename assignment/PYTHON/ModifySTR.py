ae8d00ab86bb52f4d68da6b966430b708f41ddaf
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-23 21:55:33 +0530

    modifySTR

--- assignment/PYTHON/ModifySTR.py
+++ assignment/PYTHON/ModifySTR.py
@@ -1,3 +1,12 @@
 print("MODIFYSTRINGS")
 a="hello, World"
 print(a.upper())
+b="hello, world"
+print(a.lower())
+c="hello, world"
+print(c.split())
+d="hello, world"
+print(a.replace("h","j"))
+f="hello, world"
+print(f.split(","))
+
