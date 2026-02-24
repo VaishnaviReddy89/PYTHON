26348d993fa931cd5ad50108e65784073b468419
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-24 21:28:06 +0530

    largest three numbers

--- assignment/PYTHON/streak.py
+++ assignment/PYTHON/streak.py
@@ -4,3 +4,16 @@ if num % 2 == 0:
     print("Even")
 else:
     print("Odd")
+    
+    #Find Largest of Three Numbers
+  a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print("Largest:", a)
+elif b >= a and b >= c:
+    print("Largest:", b)
+else:
+    print("Largest:", c)
+      
