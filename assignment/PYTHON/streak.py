f43c2bc5bb6dd97efe97d926236b3439b413de03
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-24 21:28:52 +0530

    streak.py

--- assignment/PYTHON/streak.py
+++ assignment/PYTHON/streak.py
@@ -6,7 +6,7 @@ else:
     print("Odd")
     
     #Find Largest of Three Numbers
-  a = int(input("Enter first number: "))
+a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 
