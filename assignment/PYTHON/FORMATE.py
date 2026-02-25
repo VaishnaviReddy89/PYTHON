2fb1b602e0431b3317d6d37997bd25d7cc82588e
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-25 22:43:00 +0530

    FORMATE

---
+++ assignment/PYTHON/FORMATE.py
@@ -0,0 +1,14 @@
+age=21
+txt=f"MY NAME IS VAISHU, I AM {age} OLD"
+print(txt)
+#f in front of the string literal, and add curly brackets {} as placeholders for variables and other operations.
+print("PLACEHOLDER AND MODIFIER")
+price=59
+txts=f"the phone price is {price}"
+print(txts)
+#A placeholder can include a modifier to format the value.
+prices=59
+txtss=f"the phone price is {prices:.2f}"
+print(txtss)
+text = f"The price is {20 * 59} dollars"
+print(text)
