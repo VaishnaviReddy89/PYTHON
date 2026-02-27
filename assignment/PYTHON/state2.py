78586e1e89abad81a5cab6b2ae27f32560694b24
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-27 22:40:27 +0530

    state2

---
+++ assignment/PYTHON/state2.py
@@ -0,0 +1,4 @@
+#A computer program is a list of "instructions" to be "executed" by a computer.
+
+#In a programming language, these programming instructions are called statements.
+print("python is fun!")
