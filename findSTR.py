dffef61992f743a9ecb31e6fdbb158f12dea6a8f
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-28 19:12:10 +0530

    findslice

--- findSTR.py
+++ findSTR.py
@@ -6,17 +6,6 @@ v="python was created by gudio van rossum"
 print(v.find('a'))
 print(v.find("a",5,8))           #[5-7]
 print(v.find("a",5,9))           #[5-8]
-
-
-
-
-
-
-
-
-
-
-#variable.find(character)
-
-#it returns the index value of first occurance of the specified character
+#variable.find(character).
+#it returns the index value of first occurance of the specified character.
 
