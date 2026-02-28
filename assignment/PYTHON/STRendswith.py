03f2428185a70bedfeec8fa35422be2655d37655
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-28 19:02:43 +0530

    STRendswith

---
+++ assignment/PYTHON/STRendswith.py
@@ -0,0 +1,13 @@
+print("STRING\n\"ENDS\tWITH\"")
+A="VAISHU REDDY"
+x=A.endswith("y")
+print(x)
+c="hello everyone myself vaishnavi"
+print(len(c))
+TXT=c.endswith("vaishnavi",21,31)
+print(TXT)
+txt="hello"
+t=txt.endswith("hello",0,5)
+print(t)
+#end index = stopping position
+#(not character position)
