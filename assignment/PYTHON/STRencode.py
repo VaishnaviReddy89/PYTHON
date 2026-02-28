2cdeb9ef3e738ffc1e52dc5466603f3425127e89
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-02-28 16:35:54 +0530

    STRencode

---
+++ assignment/PYTHON/STRencode.py
@@ -0,0 +1,7 @@
+print("STRING\t\"ENCODING\"")
+#Encoding means converting human-readable text (string) into machine-readable bytes.
+#Computers store data in binary form (0s and 1s).
+TXT="PYTHON"
+a="VAISHU"
+print(TXT.encode("utf-8"))
+print(a.encode("utf-8"))
