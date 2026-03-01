95d505226ebd91c85aa0b622c0e9297ba18a5c12
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-01 17:24:06 +0530

    STRformatemap

---
+++ assignment/PYTHON/STRformatemap.py
@@ -0,0 +1,5 @@
+insert={"name":"VAISHU","AGE":21}
+MSG="HAPPY BIRTHDAY {name} YOU ARE TURNING INTO {AGE} IN 2026"
+print(MSG.format_map(insert))
+#The format_map() method formats the specified values of a dictionary and insert them inside the string's placeholders.
+
