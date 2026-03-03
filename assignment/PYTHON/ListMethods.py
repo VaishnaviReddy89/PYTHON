71a4e0ffe8d72c83e3646f7e0b05f125a5385f4c
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-03 10:58:58 +0530

    Listchange

--- assignment/PYTHON/ListMethods.py
+++ assignment/PYTHON/ListMethods.py
@@ -2,25 +2,34 @@ print("APPEND")
 car=["BMW","FORD","FERRARI"]#add element at the end of the list
 car.append("THAR")
 print(car)
+
 car.copy()#return the list
 print(car)
+
 x=car.count("BMW")#return the no.of times the element is there in list.
 print(x)
+
 y=car.index("BMW")#return the index value of element
 print(y)
+
 cars=["m3","porsche","lamborgini"]
 car.extend(cars)
 print(car)
 
 car.insert(3,"MAHINDRA")#insert the element at particular location.
 print(car)
+
 car.pop(1)#remove the element with specified postion
 print(car)
+
 car.remove("THAR")#remove the element with specified value.
 print(car)
+
 car.reverse()#Reverse the list
 print(car)
+
 car.sort()#sort the list.
 print(car)
+
 car.clear()#clear all elements.
 print(car)
---
+++ assignment/PYTHON/Listchange.py
@@ -0,0 +1,11 @@
+market=["FRUITS","VEGETABLES","MASALA'S","DRESSES"]
+market.insert(2,"apple")
+print(market)
+market[2]="Mango"#To change the value of a specific item, refer to the index number:
+print(market)
+market[0:2]=["spoons","nuts"]
+print(market)
+market[0:5]=["panipuri"]#Change the second and third value by replacing it with one value:
+
+
+print(market)
