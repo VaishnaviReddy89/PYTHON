8c65bd448a582c9c4cd7cf425ee816d5312aa3b0
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-05 18:24:48 +0530

    pythonwhile

---
+++ assignment/PYTHON/PythonWhile.py
@@ -0,0 +1,7 @@
+print("WHILE LOOP")
+#A while loop in Python is used to repeat a block of code as long as a condition is true.
+fruits=["apple","banana","orange"]
+i=0
+while i<len(fruits):
+     print(fruits[i])
+     i=i+1
