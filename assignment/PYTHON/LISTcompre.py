95f9931a88cdf01221910b85932822829a96acab
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-05 19:01:49 +0530

    LISTcompre

---
+++ assignment/PYTHON/LISTcompre.py
@@ -0,0 +1,7 @@
+print("LIST COMPREHENSION")
+#List comprehension is a short and simple way to create lists in Python using a single line of code.
+numbers=[1,2,3,4]
+squares=[]
+for i in numbers:
+    squares.append(i+i)
+print(squares)
