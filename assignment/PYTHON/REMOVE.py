f6567b3e0b1411ae3a2e248479918b4b1c29fff5
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-05 17:59:38 +0530

    REMOVE

--- assignment/PYTHON/REMOVE.py
+++ assignment/PYTHON/REMOVE.py
@@ -1,8 +1,11 @@
 print("LIST REMOVE")
+#The remove() method removes the specified item.
 FRUITS=["APPLE","BANANA","CHERRY"]
 FRUITS.remove("BANANA")
 print(FRUITS)
 FRUITS=["APPLE","BANANA","CHERRY"]
+print("pop")
+#The pop() method removes the specified index.
 FRUITS.pop(0)
 print(FRUITS)
 FRUITS=["APPLE","BANANA","CHERRY"]
