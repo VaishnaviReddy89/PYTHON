e12c4fcb6476037d3a0040a51fae9f82010c001b
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-13 22:35:18 +0530

    ADDitemsTUPLE

---
+++ assignment/PYTHON/ADDitemsTUPLE.py
@@ -0,0 +1,6 @@
+print("ADD ITEMS")
+x=("iphone","samsung","realme")
+y=list(x)
+y[1]="redmi"
+x=tuple(y)
+print(y)
--- assignment/PYTHON/updateTUPLE.py
+++ assignment/PYTHON/updateTUPLE.py
@@ -1,5 +1,5 @@
 tuples=("apple","cherry","banana")
-y=list(tuples)
+y=list(tuples)#convert tuple into list
 y[2]="orange"
-tuples=tuple(y)
+tuples=tuple(y)#convert back into list
 print(y)
