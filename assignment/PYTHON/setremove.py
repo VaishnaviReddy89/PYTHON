f4065c68fdfbacfd99c3674028cb710bac1ea8cd
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-15 22:55:17 +0530

    setremove

---
+++ assignment/PYTHON/setremove.py
@@ -0,0 +1,11 @@
+print("SET REMOVE")
+house={"furniture","ironrods","cement","bricks","lights"}
+house.remove("bricks")
+house.discard("bricks")
+print(house)
+print(house)
+print("pop")
+department={"CSE","IT","ECE","CSM"}
+x=department.pop()
+print(x)
+print(department)
