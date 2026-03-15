2c053bb124bf42d3e4180373b6962c9e0a91b9db
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-03-15 22:48:03 +0530

    setAddtuple

---
+++ assignment/PYTHON/setAddtuple.py
@@ -0,0 +1,9 @@
+print("ADD SET ITEMS")
+gadgets={"phone","smartwatch","tab"}
+gadgets.add("pods")
+print(gadgets)
+print("To add items from another set into the current set, use the update() method.")
+accessories={"earrings","chains","barclets","hipchain"}
+makeup={"lipstick","lipbalm","eyelinear"}
+accessories.update(makeup)
+print(accessories)
