fc50a94cd42585da88ee47f17eeccfd52246450c
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-01 21:51:49 +0530

    setUpdate

---
+++ assignment/PYTHON/setUpdate.py
@@ -0,0 +1,5 @@
+IPLteams={"SRH","RCB","RR","PBKS","CSK","MI","DC"}
+#To add items from another set into the current set, use the update() method.
+ipl2={"KKR","LSG","GT"}
+IPLteams.update(ipl2)
+print(IPLteams)
