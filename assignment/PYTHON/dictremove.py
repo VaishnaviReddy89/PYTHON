8d043dcad5346ecaede22da9d5e8d89728a95380
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-05 21:41:28 +0530

    dictremove

---
+++ assignment/PYTHON/dictremove.py
@@ -0,0 +1,21 @@
+print("pop")
+dept={
+    "Name":"VAISHU",
+    "rollno":75,
+    "cgpa":8.0
+}
+dept.pop("rollno")
+print(dept)
+print("popitems")
+servey={
+    "Father":"A.RajiReddy",
+    "Mother":"A.Madhavi",
+    "Brother":"A.Rushi Kesh Reddy",
+    "Sister":"A.Vaishnavi Reddy",
+    "GrandMother":"None"
+}
+servey.popitem()
+print(servey)
+print("Clear")
+servey.clear()
+print(servey)
