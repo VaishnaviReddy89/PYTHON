0b8f3956385c7183d47f6072b7a9047a5c51d1c2
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-07 21:33:49 +0530

    ifevenorodd

--- project/ifstatements/ifevenorodd.py
+++ project/ifstatements/ifevenorodd.py
@@ -3,3 +3,8 @@ if n % 2==0:
     print("Even")
 else:
     print("Odd")
+"""
+if → checks first condition
+elif → checks another condition if previous one is False
+else → runs if all conditions are False
+"""
