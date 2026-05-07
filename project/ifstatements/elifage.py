ed369a88026fcbef9cca16d6536031bc04390608
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-07 21:42:38 +0530

    elifage

--- project/ifstatements/elifage.py
+++ project/ifstatements/elifage.py
@@ -7,4 +7,5 @@ elif age <65:
     print("You are an adult")
 elif age>=65:
     print("You are a senior")
-    
+
+#elif is used in Python to check multiple conditions after an if statement. It executes only when the previous conditions are False
