39c8beb5241416df706aa2840e603d23d83a5786
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-07 22:28:25 +0530

    elsefallback0

--- project/ifstatements/else.py
+++
@@ -1,9 +0,0 @@
-#The else statement is executed when the if condition (and any elif conditions) evaluate to False.
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
