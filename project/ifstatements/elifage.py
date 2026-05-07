a372b5b4f0ac43306dca77f99d06562bb3515910
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-07 21:37:53 +0530

    elifage

---
+++ project/ifstatements/elifage.py
@@ -0,0 +1,10 @@
+age=21
+if age<13:
+    print("you are a child")
+elif age <20:
+    print("You are a teenager")
+elif age <65:
+    print("You are an adult")
+elif age>=65:
+    print("You are a senior")
+    
