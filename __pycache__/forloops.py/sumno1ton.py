f74dd8a660fbbbc4556a8e86ea80a9b61f590d54
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-13 19:00:34 +0530

    sumno1ton

---
+++ __pycache__/forloops.py/sumno1ton.py
@@ -0,0 +1,7 @@
+n=int(input("Enter Number:"))
+sum=0#to store the total
+for i in range(1,n+1):
+    #Why n + 1?
+    #Because range() does not include the ending number.
+    sum+=i#sum = sum + i
+    print(sum)
