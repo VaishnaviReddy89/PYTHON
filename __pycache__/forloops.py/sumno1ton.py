0d50d806ad867e7960c138d165403581a968e3a9
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-05-13 19:22:51 +0530

    alltables

--- __pycache__/forloops.py/sumno1ton.py
+++ __pycache__/forloops.py/sumno1ton.py
@@ -1,3 +1,4 @@
+print("FIND SUM OF NUMBERS FROM 1 TO N")
 n=int(input("Enter Number:"))
 sum=0#to store the total
 for i in range(1,n+1):
@@ -5,3 +6,4 @@ for i in range(1,n+1):
     #Because range() does not include the ending number.
     sum+=i#sum = sum + i
     print(sum)
+#calculate the total of consecutive numbers.
