606c26e4ebcfc0c62af0da3eac718b9fa2f86a3e
VaishnaviReddy89 <252773518+VaishnaviReddy89@users.noreply.github.com>
2026-05-15 21:32:20 +0530

    parameter

--- __pycache__/forloops.py/arguments.py
+++ __pycache__/forloops.py/arguments.py
@@ -1,4 +1,5 @@
-def my_func(fname):
+def my_func(fname):#fname is the arguments
     print(fname+" Refunes ")
+    # When the function is called, we pass along a first name, which is used inside the function to print the full name:
 my_func("email")
 my_func("linus")
