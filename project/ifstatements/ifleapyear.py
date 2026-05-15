0f79e0010e0459007cfc2cd42c8afb43c1897e98
VaishnaviReddy89 <252773518+VaishnaviReddy89@users.noreply.github.com>
2026-05-15 12:07:04 +0530

    listcomprehension

--- project/ifstatements/ifleapyear.py
+++ project/ifstatements/ifleapyear.py
@@ -3,3 +3,27 @@ if(year%4==0 and year % 100!=0) or (year%400 ==0):
      print("Leap year")
 else:
     print("Not leap year")
+    '''
+    if year%4==0 and year%100!=0:
+          return True
+elif year%4==0 and year%400==0 and year%100==0:
+    return True
+else:
+     return False
+     
+ if (year % 4 == 0):
+            if (year % 100 == 0):
+            if (year % 400 == 0):
+                leap = True
+            else:
+                leap = False
+        else:
+            leap = True
+    else:
+        leap = False
+     
+    return leap
+
+     
+     '''
+     
