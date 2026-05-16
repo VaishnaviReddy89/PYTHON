8f5b399856456daf83121e6bfaec8567657ff97e
VaishnaviReddy89 <252773518+VaishnaviReddy89@users.noreply.github.com>
2026-05-16 16:09:46 +0530

    ReverseStar

---
+++ __pycache__/forloops.py/PATTERN/ReverseStar.py
@@ -0,0 +1,11 @@
+start=int(input("enter starting number : "))
+#stop=int(input("enter stoping number : "))
+end=int(input("enter ending number : "))
+for i in range(end,start-1,-1):
+    print("*" * i)
+    
+'''
+start = bigger number
+stop = smaller number
+step = -1 (backward)
+'''
