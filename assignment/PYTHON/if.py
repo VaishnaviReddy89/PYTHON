babd7ee40c06e37460b3b65d41d55304e1456d3b
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-07-16 23:46:14 +0530

    if

---
+++ assignment/PYTHON/if.py
@@ -0,0 +1,15 @@
+n=int(input())
+if(n%2 == 0):
+ print("even number")
+else:
+    print("odd number")
+n=input()
+if n>13:
+    if n>19:
+        print("teen")
+    elif n<35:
+        print("adult")
+    else:
+        print("sr")
+else:
+    print("chlid")
