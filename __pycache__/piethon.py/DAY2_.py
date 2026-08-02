de1223b47c4c0f9e45ccd7fe4d00911f9488200b
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-02 21:10:40 +0530

    DAY2._

---
+++ __pycache__/piethon.py/DAY2_.py
@@ -0,0 +1,9 @@
+print("unpackCOLLECTION")
+#PYTHON ALLOWS US TO EXTRACT the VALUES INTO VARIABLES.
+fruits=["Mango","orange","apple"]
+x,y,z=fruits
+print(x)
+a=b=c=["vaishu"]
+print(a)
+print(b)
+print(c)
