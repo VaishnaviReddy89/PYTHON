77658939f4e08618609d6a3a9d9f3fdef0e7197c
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-02 22:57:58 +0530

    DAY2LISTACCESS

--- __pycache__/piethon.py/DAY2LISTACCESS.py
+++ __pycache__/piethon.py/DAY2LISTACCESS.py
@@ -1,6 +1,10 @@
-max=["women section","kids","men section"]
-print(max[-1])
+max=["women section","kids","men section","footwear"]
+"""
+max.reverse()
+print(max)
+"""
+print(max[::-1])
 print(max[0:3])
-print(max[:4])
-print(max[4:])
+print(max[:4])#start from index 0 and index 4 is the fifth item
+print(max[4:])#empty list
 print(max[0:])
