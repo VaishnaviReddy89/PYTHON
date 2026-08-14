3c8af3def2f2629f71cc81e3c21252ecd9c3adab
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-14 19:05:31 +0530

    DAY3LISTCHANGE

---
+++ __pycache__/piethon.py/DAY3LISTCHANGE.py
@@ -0,0 +1,16 @@
+print("Change a Range of Item Values")
+phones=["iphone","oneplus","samsung","redmi","poco","vivo"]
+phones[1:3]=["nothing","realme","moto"]
+print(phones)
+#If you insert more items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly
+phone=["iphone","oneplus","samsung","redmi","poco","vivo"]
+phone[1:3]=["nothing"]
+print(phone)
+#If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
+print("Insert items")
+list=[1,2,3]
+list.insert(2,4)
+print(list)
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist) 
