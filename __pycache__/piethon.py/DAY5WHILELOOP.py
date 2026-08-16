7c66431ca51c4f60a4ae743c40329446074c797d
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-16 16:07:33 +0530

    DAY5WHILELOOP

---
+++ __pycache__/piethon.py/DAY5WHILELOOP.py
@@ -0,0 +1,13 @@
+
+#util the condition is false the loop will continue if false the loop is stop.
+"""
+i=1
+while i<=3:
+    print(i)
+    i=i+1
+    """
+list=["apple","banana","mango"]
+j=0
+while j<len(list):
+    print(list[j])
+    j=j+1
