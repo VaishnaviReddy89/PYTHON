26b41d5ce026a93d88460ce0b6bae823269cf685
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-16 15:47:23 +0530

    DAY4LOOPLIST

--- __pycache__/piethon.py/DAY4LOOPLIST.py
+++ __pycache__/piethon.py/DAY4LOOPLIST.py
@@ -1,4 +1,17 @@
 print("LOOP LIST")
 LISTs=["APPLE","ORANGE","KIWI"]
-for x in range(len(LISTs)):
-  print(LISTs[x])
+for x in LISTs:#Take each element from the LISTs list, one at a time, and temporarily store it in x.
+  print(x)# to print the items in lists.
+"""
+Here:
+LISTs = list
+x = temporary variable
+for = keyword
+in = tells Python to take values from the list
+"""
+
+number=[10,20,30,40,50]
+total=0
+for num in number:
+  total=total+num
+print(total)
