14253fa7a98a2248c465cf026140d6dfad108a44
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-16 17:12:36 +0530

    DAY5PROBLEMSINWHILE

---
+++ __pycache__/piethon.py/DAY5FORLOOP.py
@@ -0,0 +1,15 @@
+list=[1,2,3,4]
+for x in range(len(list)):
+    print(list[x])
+#An accumulator is a variable that keeps collecting a result.
+number = [10, 20, 30, 40, 50]
+total = 0
+for num in number:
+    total = total + num
+print(total)
+print("COUNTING IN A LOOP")
+num=[1,2,3,4]
+count=0
+for n in num:
+    count=count+1
+print(count)
---
+++ __pycache__/piethon.py/DAY5PROBLEMSINWHILE.py
@@ -0,0 +1,20 @@
+print("PRINT ALL ELEMENTS")
+rtrp=["b1","b2","b3"]
+p=0
+while p<len(rtrp):
+    print(rtrp[p])
+    p=p+1
+print("REVERSE THE LIST USING WHILE LOOP")
+numbers=[1,2,3,4,5]
+i=4
+while i>=0:
+    print(numbers[i])
+    i=i-1
+print("FIND THE SUM USING THE WHILE LOOP")
+NUM=[5,10,15,20]
+total=0
+i=0
+while i<=3:
+    total=total+NUM[i]
+    i=i+1
+print(total)
