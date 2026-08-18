0031a9b72b8aeb621b810617e29dc9cc3cd1d4f5
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-18 21:53:13 +0530

    DAY6COPYLIST

---
+++ __pycache__/piethon.py/DAY6COPYLIST.py
@@ -0,0 +1,12 @@
+print("COPY LIST")
+phones=["iphone","Nothing","Realme"]
+p=phones.copy()
+print(p)
+print("LIST METHOD")
+phones=["iphone","Nothing","Realme"]
+m=list(phones)
+print(m)
+print("SLICE OPERATOR")
+phones=["iphone","Nothing","Realme"]
+o=phones[:]
+print(o)
