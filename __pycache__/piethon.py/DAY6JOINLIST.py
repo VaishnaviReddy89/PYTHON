87a4a3971c5883d38984380ddbb3a203988a6b98
VaishnaviReddy89 <vaishnavireddy742@gmail.com>
2026-08-19 23:24:30 +0530

    t

--- __pycache__/piethon.py/DAY6JOINLIST.py
+++ __pycache__/piethon.py/DAY6JOINLIST.py
@@ -2,10 +2,12 @@ phones=["iphone","Nothing","Realme"]
 p=["vivo","samsung"]
 g=phones+p
 print(g)
+
 phones=["iphone","Nothing","Realme"]
 p=["vivo","samsung"]
 phones.extend(p)
 print(phones)
+
 phones=["iphone","Nothing","Realme"]
 p=["vivo","samsung"]
 for x in p:
